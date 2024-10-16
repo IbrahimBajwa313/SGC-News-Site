97d34ed710146b9f3b026a3513e6142fd96e49d7
Ibrahim Bajwa <129992649+IbrahimBajwa313@users.noreply.github.com>
2024-10-15 23:52:01 -0400

    sd

--- pages/admin/login.jsx
+++ pages/admin/login.jsx
@@ -8,7 +8,7 @@ export default function LoginPage() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const hardcodedUsername = 'ibraim';
+  const hardcodedUsername = 'admin';
   const hardcodedPassword = '123';
 
   const handleSubmit = (e) => {
